70821858c62a7ca6d4fec9e6f9129213e86f1aaf
Senay Yakut <senaykt@gmail.com>
2020-05-25 14:29:22 -0700

    Solution 1 manually checks all of the ASCII chars and convert them an upper case ASCII char but solutin 2 uses a built in C function which is declared in ctype.h library

---
+++ ASCII/capitalize.c
@@ -0,0 +1,29 @@
+/*
+Write a program which will take a string as an input and will output the uppercase of the given string
+*/
+
+# include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+#include <ctype.h>
+//Solution 1
+int main(void)
+{
+    string s = get_string("Input:");
+    printf("Output:");
+    int i;
+    int n=strlen(s);
+    for(i=0; i<n; i++)
+    {
+        if(s[i] >= "a" && s[i] <= "z")
+        {
+            printf("%c", s[i]-32);
+        }
+    }
+    printf("\n");
+}
+
+
+
+
+
