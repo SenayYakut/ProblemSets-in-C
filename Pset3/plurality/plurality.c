7c3ed78338215b7c52f049c066a65c30004889a7
Senay Yakut <senaykt@gmail.com>
2020-06-01 19:08:43 -0700

    Without modification any of entries, It prints out the election winner

--- Pset3/plurality/plurality.c
+++ Pset3/plurality/plurality.c
@@ -66,14 +66,35 @@ int main(int argc, string argv[])
 // Update vote totals given a new vote
 bool vote(string name)
 {
-    // TODO
+    for(int i = 0; i < candidates_count; i++)
+    {
+        if(strcmp(candidates[i].name, name) == 0)
+        {
+            candidates[i].votes++;
+            return true;
+        }
+    }
     return false;
 }
 
 // Print the winner (or winners) of the election
 void print_winner(void)
 {
-    // TODO
+    int highestVote = -1;
+    for(int i = 0; i < candidates[i].votes; i++)
+    {
+        if(candidates[i].votes > highestVote)
+        {
+            highestVote = candidates[i].votes;
+        }
+    }
+    for(int i = 0; i < candidates_count; i++)
+    {
+        if(candidates[i].votes == highestVote)
+        {
+            printf("%s\n", candidates[i].name);
+        }
+    }
     return;
 }
 
