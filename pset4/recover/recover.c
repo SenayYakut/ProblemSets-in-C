b8fd441041fdd26c2ab3747f94ed0f5f5d1ec57e
Senay Yakut <senaykt@gmail.com>
2020-06-14 19:33:02 -0700

    Semi solution for recover algorithm

--- pset4/recover/recover.c
+++ pset4/recover/recover.c
@@ -1,7 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include <stdio.h>
+#include <string.h>
+
+
+
 int main(int argc, char *argv[])
 {
+    // Check user typed at least 2 words 
+    if (argc != 2)
+    {
+        return 1;
+    }
 
+    // Open file
+    FILE *file = fopen(argv[1], "r");
+    if (file == NULL)
+    {
+        return 1;
+    }
+
+    // Close file
+    unsigned char bytes[3];
+    fread(bytes, 3, 1, file);
+    
+    if(bytes[0] == 0xff && bytes[1] == 0xd8 && bytes[2] == 0xff)
+    {
+        printf("Maybe\n");
+    }
+    else
+    {
+        printf("Different\n");
+    }
 }
