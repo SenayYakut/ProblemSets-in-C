5e85b4738d86a979586d3057c2d399fb5147ec70
Senay Yakut <senaykt@gmail.com>
2020-06-16 12:57:54 -0700

    Open the memory card and find a jpeg file

--- pset4/recover/recover.c
+++ pset4/recover/recover.c
@@ -1,20 +1,23 @@
 #include <stdio.h>
-#include <stdlib.h>
-
-#include <stdio.h>
 #include <string.h>
+#include <cs50.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
 
 
-
+#define BLOCKSIZE 512
+typedef uint8_t BYTE;
 int main(int argc, char *argv[])
 {
-    // Check user typed at least 2 words 
+    // Accepting exactly one command-line argument 
     if (argc != 2)
     {
+        printf("Usage: ./recover image\n");
         return 1;
     }
 
-    // Open file
+    // Open memory card
     FILE *file = fopen(argv[1], "r");
     if (file == NULL)
     {
@@ -22,15 +25,22 @@ int main(int argc, char *argv[])
     }
 
     // Close file
-    unsigned char bytes[4];
-    fread(bytes, 3, 1, file);
+    //unsigned char bytes[4];
     
-    if(bytes[0] == 0xff && bytes[1] == 0xd8 && bytes[2] == 0xff && (bytes[3] & 0xf0) == 0xe0)
+    int *bytes = malloc(128 * sizeof(int));
+    fread(bytes, 512, 1, file);
+    
+ 
+    if (bytes[0] == 0xff && bytes[1] == 0xd8 && bytes[2] == 0xff && (bytes[3] & 0xf0) == 0xe0)
     {
-        printf("Maybe\n");
+        sprintf("bytes", "%03i.jpg", 2);
+        FILE *img = fopen("bytes", "w");
+        
     }
-    else
-    {
-        printf("Different\n");
-    }
+    
+    
+
+    //Untill when you should repeat
+    
+    fclose(file);
 }
