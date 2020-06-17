a2aa1c14a6db5e88091eaf7db10a8e6cd61b1d9f
Senay Yakut <senaykt@gmail.com>
2020-06-17 16:03:45 -0700

    recovers JPEGs from a forensic image Algorithm in C

--- pset4/recover/recover.c
+++ pset4/recover/recover.c
@@ -1,46 +1,72 @@
 #include <stdio.h>
 #include <string.h>
-#include <cs50.h>
+// #include <cs50.h>
 #include <stdlib.h>
 #include <stdint.h>
 #include <stdbool.h>
 
-
-#define BLOCKSIZE 512
 typedef uint8_t BYTE;
+#define BLOCK_SIZE 512
+#define FILE_NAME_SIZE 8
+bool is_start_new_jpeg(BYTE buffer[]);
+
 int main(int argc, char *argv[])
 {
-    // Accepting exactly one command-line argument 
+    // Make sure Accepting one  command-line argument 
     if (argc != 2)
     {
         printf("Usage: ./recover image\n");
         return 1;
     }
-
     // Open memory card
-    FILE *file = fopen(argv[1], "r");
-    if (file == NULL)
+    FILE *infile = fopen(argv[1], "r");
+    //make sure it isnt empthy
+    if (infile == NULL)
     {
+        printf("File is not found\n");
         return 1;
     }
-
-    // Close file
-    //unsigned char bytes[4];
-    
-    int *bytes = malloc(128 * sizeof(int));
-    fread(bytes, 512, 1, file);
-    
- 
-    if (bytes[0] == 0xff && bytes[1] == 0xd8 && bytes[2] == 0xff && (bytes[3] & 0xf0) == 0xe0)
+//define a buffer
+    BYTE buffer[BLOCK_SIZE];
+    int file_index = 0;
+    bool have_found_first_jpg = false;
+    FILE *outfile;//define a file pointer outfile
+    //read from card.raw to buffer
+    while (fread(buffer, BLOCK_SIZE, 1, infile))
     {
-        sprintf("bytes", "%03i.jpg", 2);
-        FILE *img = fopen("bytes", "w");
-        
+        if (is_start_new_jpeg(buffer))
+        {
+            if (!have_found_first_jpg)
+            {
+                have_found_first_jpg = true;
+            }
+            else
+            {
+                fclose(outfile);
+            }
+            
+               
+            char filename[FILE_NAME_SIZE];
+            sprintf(filename, "%03i.jpg", file_index++);
+            outfile = fopen(filename, "w");
+            if (outfile == NULL)
+            {
+                return 1;
+            }
+            fwrite(buffer, BLOCK_SIZE, 1, outfile);
+        }
+        else if (have_found_first_jpg)
+        {
+            //
+            fwrite(buffer, BLOCK_SIZE, 1, outfile);
+
+        }
     }
     
-    
-
-    //Untill when you should repeat
-    
-    fclose(file);
-}
+    fclose(outfile);
+    fclose(infile);
+}    
+bool is_start_new_jpeg(BYTE buffer[])    
+{ 
+    return buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0;
+}      
