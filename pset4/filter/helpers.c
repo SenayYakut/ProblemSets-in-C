7732736abb0d976c1ddde3c8ee83cabbeb08e140
Senay Yakut <senaykt@gmail.com>
2020-06-20 14:14:12 -0700

    Image filtering algorithm gray scale solution

--- pset4/filter/helpers.c
+++ pset4/filter/helpers.c
@@ -3,8 +3,29 @@
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
-    return;
+    for(int i = 0; i < height; i++)
+    {
+        for(int j = 0; j < width; j++)
+        {
+            //Store all the current values in the variables
+            int blue = image[i][j].rgbtBlue;
+            int green = image[i][j].rgbtGreen;
+            int red = image[i][j].rgbtRed;
+        
+    
+            //find the average value
+            int average = round((red+green+blue)/3.0);
+             //set the current values of all pxels to the average
+            image[i][j].rgbtBlue = average;
+            image[i][j].rgbtGreen = average;
+            image[i][j].rgbtRed = average;
+
+        }
+    }
+
 }
+
+
 
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
