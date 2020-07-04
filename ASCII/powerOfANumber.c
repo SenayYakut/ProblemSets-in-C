505fdc8dc7f0e78b075da5a4cbae578ef65f0cb4
Senay Yakut <senaykt@gmail.com>
2020-07-04 09:52:58 -0700

    solution 1 using while loop, solution 2 iss pow function

--- ASCII/powerOfANumber.c
+++ ASCII/powerOfANumber.c
@@ -3,6 +3,7 @@
 */
 
 #include <stdio.h>
+#include <math.h>
 
 int main()
 {
@@ -21,5 +22,21 @@ int main()
         exp--;
     }
     printf("Answer is %lld\n", result);
+    return 0;
+}
+//Solution 2 is using built in pow function which is declared in math.h standard library
+
+#include <stdio.h>
+#include <math.h>
+int main(void)
+{
+    double base, exp, result;
+    printf("Enter a base number: ");
+    scanf("%ld", &base);
+    printf("Enter an exponent number: ");
+    scanf("%ld", &exp);
+
+    result = pow(base, exp);
+    printf("%.1ld ^ %.1ld is %.2ld", base, exp, result);
     return 0;
 }
