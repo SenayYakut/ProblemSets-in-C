ced022f2664a5da5ed0c0a5071b7b13314cb677a
Senay Yakut <senaykt@gmail.com>
2020-07-03 18:59:17 -0700

    Lowest common multiple Algorith

---
+++ ASCII/lcm.c
@@ -0,0 +1,24 @@
+/*
+Write a program in C to find lowest common of two numbers
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+    int n1, n2, min;
+    printf("Enter 2 positive numbers: ");
+    scanf("%d %d", &n1, &n2);
+    min = (n1 > n2)? n1 : n2;
+
+    while(1)
+    {
+    if(min % n1 == 0 && min % n2 ==0)
+    {
+        printf("Lowest common multiple for %d and %d is %d ", n1, n2, min);
+        break;
+    }
+    min++;
+    }
+    return 0;
+}
