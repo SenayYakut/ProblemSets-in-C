1e030002ce7cae19ee010faf6f961c9f195555ac
Senay Yakut <senaykt@gmail.com>
2020-07-04 17:10:10 -0700

    Simple Calculator in C

---
+++ calculator/calculator.c
@@ -0,0 +1,5 @@
+/*
+Write a program in C which takes an arithmetic operator +, -, *, / and two operands from the user. Then, it performs the calculation on the two operands depending upon the operator entered by the user.
+
+
+*/
